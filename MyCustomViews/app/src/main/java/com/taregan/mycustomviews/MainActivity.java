05aa43a6f8862a3155352cea6aa19e7e2eb7f0fb
Pitambar Koirala <er.pitambar48@gmail.com>
2018-04-02 08:39:51 +0545

    initial commit

---
+++ MyCustomViews/app/src/main/java/com/taregan/mycustomviews/MainActivity.java
@@ -0,0 +1,22 @@
+package com.taregan.mycustomviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO 1: create new package - views
+    //TODO 2: create custom view class 'MyCustomButton' by extending AppCompactButton
+    //TODO 3: define constructors in MyCustomButton Class
+    //TODO 4: override onDraw Method
+    //TODO 5: create drawable resources for background drawable
+    //TODO 6: setBackgroundDrawable to custombutton
+    //TODO 7: Specify custom attributes in attrs.xml
+    //TODO 8: use specified attributes in MyCustomButtonClass
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
